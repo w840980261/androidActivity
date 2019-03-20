a8d1d1bd117071c87a4891dc66a903e722a1edc2
wyq <840980261@qq.com>
2019-03-20 20:36:33 +0800

    多个activity交互

--- app/src/main/java/com/example/wyq/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/wyq/myapplication/Main2Activity.java
@@ -1,65 +1,53 @@
 package com.example.wyq.myapplication;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity {
 
     public static final String TAG="msg:";
 
-    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Log.i(TAG,"MainActivity onCreate");
-        button=(Button)findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(MainActivity.this,Main2Activity.class);
-                startActivity(intent);
-            }
-        });
+        setContentView(R.layout.activity_main2);
+        Log.i(TAG,"Main2Activity onCreate");
     }
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.i(TAG,"MainActivity onRestart");
+        Log.i(TAG,"Main2Activity onRestart");
         // The activity is about to become visible.
     }
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i(TAG,"MainActivity onStart");
+        Log.i(TAG,"Main2Activity onStart");
         // The activity is about to become visible.
     }
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i(TAG,"MainActivity onResume");
+        Log.i(TAG,"Main2Activity onResume");
         // The activity has become visible (it is now "resumed").
     }
     @Override
     protected void onPause() {
         super.onPause();
-        Log.i(TAG,"MainActivity onPause");
+        Log.i(TAG,"Main2Activity onPause");
         // Another activity is taking focus (this activity is about to be "paused").
     }
     @Override
     protected void onStop() {
         super.onStop();
-        Log.i(TAG,"MainActivity onStop");
+        Log.i(TAG,"Main2Activity onStop");
         // The activity is no longer visible (it is now "stopped")
     }
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.i(TAG,"MainActivity onDestroy");
+        Log.i(TAG,"Main2Activity onDestroy");
         // The activity is about to be destroyed.
     }
 }
